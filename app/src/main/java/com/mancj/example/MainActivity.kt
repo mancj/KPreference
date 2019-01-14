8639d9608746be2362f7bcd570a64c0a14a98ee1
Mansur <mancjone@gmail.com>
2019-01-14 18:05:48 +0300

    Allow custom preference name; Fix string nullable bug;

--- app/src/main/java/com/mancj/example/MainActivity.kt
+++ app/src/main/java/com/mancj/example/MainActivity.kt
@@ -6,6 +6,7 @@ import android.preference.PreferenceManager
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import com.mancj.kpreference.BooleanPreference
+import com.mancj.kpreference.IntPreference
 import com.mancj.kpreference.PreferenceHolder
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,6 +14,11 @@ class MainActivity : AppCompatActivity(), PreferenceHolder {
     override val preferences: SharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
     val isFirstLaunch by BooleanPreference(false)
+    val customNamePreference by IntPreference(2, PREFERENCE_USER_AGE)
+
+    companion object {
+        private const val PREFERENCE_USER_AGE = "PREFERENCE_USER_AGE"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- kpreference/src/main/java/com/mancj/kpreference/KPreference.kt
+++ kpreference/src/main/java/com/mancj/kpreference/KPreference.kt
@@ -3,23 +3,27 @@ package com.mancj.kpreference
 import android.content.SharedPreferences
 import kotlin.reflect.KProperty
 
-open class KPreference<T>(private val defVal: T?, private val clazz: Class<T>) {
+open class KPreference<T>(
+    private val defVal: T?,
+    private val preferenceName: String? = null,
+    private val clazz: Class<T>
+) {
 
     @Suppress("UNCHECKED_CAST")
-    operator fun getValue(thisRef: PreferenceHolder, property: KProperty<*>): T = with(thisRef.preferences) {
-        val key = property.name
+    open operator fun getValue(thisRef: PreferenceHolder, property: KProperty<*>): T = with(thisRef.preferences) {
+        val key = preferenceName ?: property.name
         when (clazz) {
             Int::class.java -> getInt(key, defVal as Int) as T
             Long::class.java -> getLong(key, defVal as Long) as T
             Float::class.java -> getFloat(key, defVal as Float) as T
-            String::class.java -> getString(key, defVal as String) as T
+            String::class.java -> getString(key, (defVal as? String)) as T
             Boolean::class.java -> getBoolean(key, defVal as Boolean) as T
             else -> null!!
         }
     }
 
-    operator fun setValue(thisRef: PreferenceHolder, property: KProperty<*>, value: T) {
-        val key = property.name
+    open operator fun setValue(thisRef: PreferenceHolder, property: KProperty<*>, value: T) {
+        val key = preferenceName ?: property.name
         thisRef.preferences.edit().run {
             when (clazz) {
                 Int::class.java -> putInt(key, value as Int)
@@ -37,8 +41,17 @@ interface PreferenceHolder {
     val preferences: SharedPreferences
 }
 
-class IntPreference(defVal: Int) : KPreference<Int>(defVal, Int::class.java)
-class LongPreference(defVal: Long) : KPreference<Long>(defVal, Long::class.java)
-class FloatPreference(defVal: Float) : KPreference<Float>(defVal, Float::class.java)
-class BooleanPreference(defVal: Boolean) : KPreference<Boolean>(defVal, Boolean::class.java)
-class StringPreference(defVal: String? = null) : KPreference<String>(defVal, String::class.java)
+class IntPreference(defVal: Int, preferenceName: String? = null) :
+    KPreference<Int>(defVal, preferenceName, Int::class.java)
+
+class LongPreference(defVal: Long, preferenceName: String? = null) :
+    KPreference<Long>(defVal, preferenceName, Long::class.java)
+
+class FloatPreference(defVal: Float, preferenceName: String? = null) :
+    KPreference<Float>(defVal, preferenceName, Float::class.java)
+
+class BooleanPreference(defVal: Boolean, preferenceName: String? = null) :
+    KPreference<Boolean>(defVal, preferenceName, Boolean::class.java)
+
+class StringPreference(defVal: String? = null, preferenceName: String? = null) :
+    KPreference<String>(defVal, preferenceName, String::class.java)
